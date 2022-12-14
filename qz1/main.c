6cca7fb322de4b6b2be497644898a4b6fc30f349
Lyc0326 <90496853+Lyc0326@users.noreply.github.com>
2022-12-14 15:50:14 +0800

    Add files via upload

---
+++ qz1/main.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("%.*s\n",5,"*****");
+    printf("%.*s\n",5,"*****");
+    printf("%.*s\n",5,"*****");
+    printf("%.*s\n",5,"*****");
+    printf("%.*s\n",5,"*****");
+
+    return 0;
+}
