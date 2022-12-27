28b2ecfedf59603290650076a141a48d152bed1a
Lyc0326 <90496853+Lyc0326@users.noreply.github.com>
2022-12-27 23:00:37 +0800

    Add files via upload

---
+++ HW6/main1.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    
+    int i = 10;
+    if(i % 15 == 0)
+        printf("Love IU\n");
+        
+    else if(i % 3 == 0)
+        printf("Love\n");
+  
+    else if(i % 5 == 0)
+        printf("IU\n");
+
+    else
+        printf("%d\n",i);
+
+    return 0;
+}
+
+
+
+
+
